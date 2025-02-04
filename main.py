eabf1735091e0a8e20ebb0dce1f69cf2895ae422
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2025-02-04 18:20:45 -0300

    refactor: change flask for fastapi

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import logging
 import os
 import getpass
 from langchain_openai import ChatOpenAI
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from dotenv import load_dotenv
 
 logger = logging.getLogger(__name__)
@@ -12,22 +13,26 @@ load_dotenv()
 if not os.environ.get("OPENAI_API_KEY"):
     os.environ["OPEN_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
 
-app = Flask(__name__)
+app = FastAPI()
 
 chat_model = ChatOpenAI(model="gpt-4", api_key=os.environ["OPENAI_API_KEY"])
 
 
-@app.route("/talking_ia", methods=["POST"])
-def text_ai():
-    user_text = request.json.get("text")
-
-    if not user_text:
-        return jsonify({"error": "Text not provided"}), 400
-
-    result = chat_model.invoke(input=user_text)
-
-    return jsonify({"response": str(result.content)})
+class QuestionRequest(BaseModel):
+    text: str
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.post("/talking_ai")
+def text_ai(request: QuestionRequest):
+    try:
+        user_text = request.text
+
+        if not user_text:
+            return HTTPException(status_code=400, detail="Text not provided")
+
+        result = chat_model.invoke(input=user_text)
+
+        return ({"response": str(result.content)})
+    except Exception as ex:
+        logger.error(f"Error whike asking question: {ex}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
